03b4ed2271c852ab0169f84fac8c75e0ff309ad9
yjj8353 <yjj8353@gmail.com>
2024-08-15 16:02:47 +0900

    sonarqube jdk8 지원 버전으로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("jvm") version "2.0.0"
     `maven-publish`
     id("org.jetbrains.dokka") version "1.9.20"
-    id("org.sonarqube") version "3.5.0.2730"
+    id("org.sonarqube") version "4.0.0.2929"
 }
 
 jacoco {
