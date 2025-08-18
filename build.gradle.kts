1e644a96e3109695205e853dd7ddc2d4871f7aff
yjj8353 <yjj8353@gmail.com>
2025-08-18 13:04:14 +0900

    라이브러리 버전 업

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,8 @@ repositories {
     mavenCentral()
 }
 
-val cryptography = "0.51.0-alpha"
-val dataUtils = "0.23.0-alpha"
+val cryptography = "0.51.2-alpha"
+val dataUtils = "0.23.7-alpha"
 val lombok = "1.18.38"
 val tika = "2.9.4" // tika 3.0.0 부터 java 11을 요구하므로 바꾸지 말 것
 val poi = "5.4.1"
